e17af301db146b126be1c46602d3eb6ccbd23db6
Anne-gaelle BONNEFOY <abonnefo@paul-f5Ar3s4.clusters.42paris.fr>
2024-05-28 11:23:17 +0200

    setup files

--- includes/main.hpp
+++ includes/main.hpp
@@ -6,15 +6,15 @@
 #include "IRCServ.hpp"
 
 #include <iostream>
-#include <vector> //-> for vector
-#include <sys/socket.h> //-> for socket()
-#include <sys/types.h> //-> for socket()
-#include <netinet/in.h> //-> for sockaddr_in
-#include <fcntl.h> //-> for fcntl()
-#include <unistd.h> //-> for close()
-#include <arpa/inet.h> //-> for inet_ntoa()
-#include <poll.h> //-> for poll()
-#include <csignal> //-> for signal()
+#include <vector>		//-> for vector
+#include <sys/socket.h>	//-> for socket()
+#include <sys/types.h>	//-> for socket()
+#include <netinet/in.h>	//-> for sockaddr_in
+#include <fcntl.h>		//-> for fcntl()
+#include <unistd.h>		//-> for close()
+#include <arpa/inet.h>	//-> for inet_ntoa()
+#include <poll.h>		//-> for poll()
+#include <csignal>		//-> for signal()
 
 #endif // MAIN_HPP
 
